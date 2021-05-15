778f8ed833c457be983a49ee29db29b2d052852d
admiralpxl <wnstnjr@gmail.com>
2021-05-15 14:24:51 -0400

    logica basica terminada

--- index.js
+++ index.js
@@ -1,53 +1,45 @@
 const rango = document.getElementById('slider');
-const valor = document.getElementById('valor');
-const pages = document.getElementById('pages')
+const valores = document.getElementById('valor');
+const pages = document.getElementById('pages');
+const check = document.getElementById('check');
+const porcentaje = .25;
+let precio = [8, 12, 16, 24, 36];
+let anual = precio.map( (x) => x * porcentaje);
+let cantidad = ['10K','50K','100K','500K','1M']
 
 
 addEventListener('load',inicio,false); 
 
 
+const texto = (lugar,valor) =>  lugar.innerHTML = valor;
+
+function cambiodelvalor(){
+    if(check.checked == false)
+    for(let i = 0; i <= precio.length; i++){
+        if(rango.value == i + 1){
+            texto(valores,`$${precio[i]}.00`);
+        }
+    } else {
+        for(let i = 0; i <= anual.length; i++){
+            if(rango.value == i + 1){
+                texto(valores,`$${anual[i]}.00`);
+            }
+    }
+}
+}
+function cambiodepaginas(){
+    for(let i = 0; i <= precio.length; i++){
+        if(rango.value == i + 1){
+            texto(pages,cantidad[i])
+        }
+    }
+}
+function cambiodevalor(){
+    cambiodelvalor();
+    cambiodepaginas();
+}
+
 function inicio() {
     rango.addEventListener('touchmove',cambiodevalor,false);
     rango.addEventListener('mousemove',cambiodevalor,false);
 }
-
-function cambiodevalor(){
-    //valor.innerHTML = rango.value;
-    cambiodelvalor();
-    cambiodepaginas();
-}
-function cambiodelvalor(){
-    if(rango.value == 1){
-        valor.innerHTML = '$8.00';
-    }
-    if(rango.value == 2){
-        valor.innerHTML = '$12.00';
-    }
-    if(rango.value == 3){
-        valor.innerHTML = '$16.00';
-    }
-    if(rango.value == 4){
-        valor.innerHTML = '$24.00';
-    }
-    if(rango.value == 5) {
-        rango.innerHTML = '$36.00';
-    }
-}
-function cambiodepaginas(){
-    if(rango.value == 1){
-        pages.innerHTML = '10k';
-    }
-    if(rango.value == 2){
-        pages.innerHTML = '50k';
-    }
-    if(rango.value == 3){
-        pages.innerHTML = '100k';
-    }
-    if(rango.value == 4){
-        pages.innerHTML = '500k';
-    }
-    if(rango.value == 5) {
-        pages.innerHTML = '1M';
-    }
-}
-
